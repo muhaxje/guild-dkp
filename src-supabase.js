4bf79fc4af0298e8d98f26d8b9c793b0bc0264f1
muhaxje <muhaxje@gmail.com>
2026-05-03 16:33:33 +0200

    Delete src-supabase.js

--- src-supabase.js
+++
@@ -1,5 +0,0 @@
-mport { createClient } from '@supabase/supabase-js'
-export const supabase = createClient(
-  'https://scmnbypdyazarlamjsos.supabase.co/rest/v1/', 
-  'scmnbypdyazarlamjsos'           
-)
